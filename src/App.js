b3b5906d7001d0f744b105dc50c6d6ae05c1948c
sawgzq <sawgzq@mail.missouri.edu>
2021-01-12 15:15:40 -0600

    to desktop

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import logo from './logo.png';
-import './App.css';
-import React, {Component} from 'react';
+import logo from "./logo.png";
+import "./App.css";
+import React, { Component } from "react";
 
-import Search from './Search'
+import Search from "./Search";
 
-class App extends Component{
+class App extends Component {
   synth = window.speechSynthesis;
-  
-  paws(){
-    console.log('clicked!');
+
+  paws() {
+    console.log("clicked!");
   }
-  render(){
-    if ('speechSynthesis' in window) {
+  render() {
+    if ("speechSynthesis" in window) {
       // Speech Synthesis supported 🎉
       return (
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" style={{"pointerEvents": "all"}} onClick={() => this.synth.paused ?   this.synth.resume(): this.synth.pause()} alt="logo" />
-            <p>
-              This is ReditReader! Click Snuu to pause and play!
-            </p>
+            <img
+              src={logo}
+              className="App-logo"
+              style={{ pointerEvents: "all" }}
+              onClick={() =>
+                this.synth.paused ? this.synth.resume() : this.synth.pause()
+              }
+              alt="logo"
+            />
+            <p>This is ReditReader! Click Snuu to pause and play!</p>
             <Search></Search>
           </header>
         </div>
-    )
-     }else{
-       // Speech Synthesis Not Supported 😣
-       alert("Sorry, your browser doesn't support text to speech!");
-       return(
+      );
+    } else {
+      // Speech Synthesis Not Supported 😣
+      alert("Sorry, your browser doesn't support text to speech!");
+      return (
         <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"  />
-          <p>
-            Sorry, your browser doesn't support text to speach!
-          </p>
-        </header>
-      </div>
-       )
-     }
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Sorry, your browser doesn't support text to speach!</p>
+          </header>
+        </div>
+      );
+    }
   }
 }
 
